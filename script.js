6e808387d091fd278f3c64b1ac82af33b0563bc0
Auutumn <117687231+Auutumn@users.noreply.github.com>
2024-08-30 00:01:22 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("sideMenu").style.width = "250px";
+  document.getElementById("mainContent").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("sideMenu").style.width = "0";
+  document.getElementById("mainContent").style.marginLeft= "0";
+}
