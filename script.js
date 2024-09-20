1a36634760be7c9423a8d44ae730dfb05a161e64
Auutumn <117687231+Auutumn@users.noreply.github.com>
2024-09-20 19:11:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function closeNav() {
   document.getElementById("sideMenu").style.width = "0";
   document.getElementById("mainContent").style.marginLeft= "0";
 }
+
+window.onload = function() {
+  setTimeout(function() {
+    const pantallaCarga = document.getElementById("pantallaCarga");
+    pantallaCarga.style.display = "none";
+  }, 1500); 
+};
